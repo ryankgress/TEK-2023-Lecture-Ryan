a25d3ec010a4082ea5a64224b828fa8914a228ff
ryankgress <ryankgress@gmail.com>
2023-02-09 16:00:45 -0500

    Coffee Part 1 done

--- ClassExamples/src/com/tek/java/coffee/CoffeeShop.java
+++ ClassExamples/src/com/tek/java/coffee/CoffeeShop.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Will contain all functionality for working with out coffee shop. 
+ * Will contain all functionality for working with our coffee shop. 
  * @author Ryan Gress
  *
  */
@@ -17,21 +17,32 @@ public class CoffeeShop {
 		MenuItem item1 = new MenuItem();
 		item1.setName("Small Coffee");
 		item1.setPrice(4.30);
+		item1.setInStock(20);
 		
 		MenuItem item2 = new MenuItem();
 		item2.setName("Large Coffee");
 		item2.setPrice(5.99);
+		item2.setInStock(75);
+		
+		MenuItem item3 = new MenuItem("Choc Chip Cookie", 4.25, 30);
 		
 		menuItems.add(item1);
 		menuItems.add(item2);
+		menuItems.add(item3);
+
+		// Reducing the item addition to one line
+		menuItems.add(new MenuItem("Egg Sandwich", 14.30, 10));
+		
 	}
 	
 	public void printMenuItems() {
 		String t = "###,###.00";
 		DecimalFormat moneyFormat = new DecimalFormat(t);
 		
+		System.out.println("Item\t\t\tStock\t\tPrice");
+		
 		for( MenuItem item : menuItems) {
-			System.out.println(item.getName() + "\t$" + moneyFormat.format(item.getPrice()));
+			System.out.println(item.getName() + "      \t" + item.getInStock() + "\t\t$" + moneyFormat.format(item.getPrice()));
 		}
 	}
 }
--- ClassExamples/src/com/tek/java/coffee/MenuItem.java
+++ ClassExamples/src/com/tek/java/coffee/MenuItem.java
@@ -4,6 +4,24 @@ public class MenuItem {
 	
 	private String name;
 	private double price;
+	private int inStock;
+	
+	// Default Constructor
+	public MenuItem() {
+		
+	}
+	
+	// Constructor with name & price
+	public MenuItem(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public MenuItem(String name, double price, int inStock) {
+		this.name = name;
+		this.price = price;
+		this.inStock = inStock;
+	}
 	
 	public String getName() {
 		return name;
@@ -17,6 +35,13 @@ public class MenuItem {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	public int getInStock() {
+		return inStock;
+	}
+	public void setInStock(int inStock) {
+		this.inStock = inStock;
+	}
+	
 	
 	
 }
