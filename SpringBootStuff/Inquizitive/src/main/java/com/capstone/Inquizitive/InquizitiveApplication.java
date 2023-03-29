9447f63a3590a745aac3f33dd03f4bf4784d92db
ryankgress <ryankgress@gmail.com>
2023-03-29 09:23:18 -0400

    Inquizitive Maven Project Started

---
+++ SpringBootStuff/Inquizitive/src/main/java/com/capstone/Inquizitive/InquizitiveApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.Inquizitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquizitiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquizitiveApplication.class, args);
+	}
+
+}
---
+++ SpringBootStuff/Inquizitive/src/test/java/com/capstone/Inquizitive/InquizitiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.Inquizitive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquizitiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
